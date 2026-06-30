3bfa498255eb57d953ff2fdf7647f2fc46fc6a85
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2026-06-30 15:46:29 +0200

    start mission c

---
+++ mission_c/smooth_motor.py
@@ -0,0 +1,50 @@
+def map(x,in_min,in_max,out_min,out_max):
+  return (x - in_min)/(in_max - in_min) *(out_max - out_min) +out_min
+
+class SmoothMotor:
+    def __init__(self, motor, acceleration = 1):
+        self.speed = 0
+        self.target_speed = 0
+        self.direction = 1
+        self.motor = motor
+        self.acceleration = acceleration
+
+    def stop(self):
+        self.motor.throttle = 0
+        self.speed = 0
+        self.target_speed = 0
+
+    def accelerate_to(self, speed, direction, acceleration=1):
+        self.acceleration = acceleration
+        if direction != -1 and direction != 1:
+            direction = 1
+        if speed > 100:
+            speed = 100
+        if speed < 0:
+            speed = 0
+        self.direction = direction
+        self.target_speed = speed * direction
+
+    def set_speed(self, speed, direction):
+        if direction != -1 and direction != 1:
+            direction = 1
+        if speed > 100:
+            speed = 100
+        if speed < 0:
+            speed = 0
+        self.direction = direction
+        self.target_speed = speed * direction
+        self._set_speed_immediate(speed * direction)
+    
+    def _set_speed_immediate(self, speed):
+        self.speed = speed
+        self.motor.throttle = self.speed / 100
+
+    def destroy(self):
+        self.stop()
+        
+    def update_speed(self):
+        if self.speed < self.target_speed:
+            self._set_speed_immediate(min(self.speed + self.acceleration, self.target_speed))
+        elif self.speed > self.target_speed:
+            self._set_speed_immediate(max(self.speed - self.acceleration, self.target_speed))
